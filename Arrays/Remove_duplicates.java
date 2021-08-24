e1bed871adadeb75aaba78a2217a507323f8f20d
Arpita Verma <arpitaverma8885@gmail.com>
2021-08-24 13:23:50 +0530

    Update Remove_duplicates.java

---
+++ Arrays/Remove_duplicates.java
@@ -0,0 +1,23 @@
+/* Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums. */
+
+[Link to the Question](https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/)
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length==0)
+            return 0;
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+        
+    }
+}
