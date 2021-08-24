8cbf76d1e77cd84b049bca3ba0a942e39fdac26a
Arpita Verma <arpitaverma8885@gmail.com>
2021-08-24 13:53:24 +0530

    Update Buy_Sell_stock.java

--- Arrays/Buy_Sell_stock.java
+++ Arrays/Buy_Sell_stock.java
@@ -1,6 +1,7 @@
 /* You are given an array prices where prices[i] is the price of a given stock on the ith day.
 Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times). */
 
+[Link to the question](https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/)
 
 class Solution {
     public int maxProfit(int[] prices) {
