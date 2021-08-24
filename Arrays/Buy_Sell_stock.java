c702f770b4b2ca997e7055dc8e3256e73b0648a7
Arpita Verma <arpitaverma8885@gmail.com>
2021-08-24 13:52:19 +0530

    Create Buy_Sell_stock.java

---
+++ Arrays/Buy_Sell_stock.java
@@ -0,0 +1,15 @@
+/* You are given an array prices where prices[i] is the price of a given stock on the ith day.
+Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times). */
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]>prices[i-1]){
+                maxProfit+=prices[i]-prices[i-1];
+            }
+        }
+       return maxProfit;
+    }
+}
